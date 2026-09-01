9b0d6150388af8a1a2cd55949d8a5cb64e2f736e
Gabriel Rivera <kitsunekoge@gmail.com>
2026-09-01 13:07:20 -0600

    Fix formatting of output in Ejercicio3.java

--- P2/Ejercicio3.java
+++ P2/Ejercicio3.java
@@ -3,10 +3,10 @@ public class Ejercicio3 {
 
         int n;
 
-        System.out.println("n    10*n    100*n    1000*n");
+        System.out.println("n\t10*n\t100*n\t1000*n");
 
         for (n = 1; n <= 5; n++) {
-            System.out.println(n + "    " + 10 * n + "    " + 100 * n + "    " + 1000 * n);
+            System.out.println(n + "\t" + 10 * n + "\t" + 100 * n + "\t" + 1000 * n);
         }
     }
 }
