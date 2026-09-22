347030801ce6730ef62be293a7e1e5cccf576c76
Zidos0 <vitalherreracesarantonio@gmail.com>
2026-09-22 15:54:53 -0600

    Add files via upload

---
+++ P4/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[]args){
+   Scanner scanner = new Scanner(System.in);
+
+
+   System.out.println("Ingresa la oracion a analizar");
+   String texto = scanner.nextLine();
+
+   AnalizadorPalabras analizador = new AnalizadorPalabras(texto);
+
+   analizador.contarPalabras();
+
+   int cantidadDuplicados = analizador.obtenerNumeroDuplicado();
+   System.out.println("\nPalabras diferentes que se duplicaron: " + cantidadDuplicados + "\n");
+   
+   System.out.println("Detalle de duplicados(Sin ordenar)");
+   analizador.mostrarDuplicadas(false);
+
+   System.out.print                                                             ln("\nDetalle de duplicados (Orden alfabetico)");
+   analizador.mostrarDuplicadas(true);
+
+   scanner.close();
+
+
+  }
+
+}
